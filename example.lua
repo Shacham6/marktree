e91a462b9bc8e01e65444d321fcb8b29810a0ffe
Shacham Ginat <shachamginat@Shachams-MacBook-Pro.local>
2024-12-21 11:06:05 +0200

    Hmm

--- example.lua
+++ example.lua
@@ -6,20 +6,27 @@ local current_window = vim.api.nvim_get_current_win()
 local current_height = vim.api.nvim_win_get_height(current_window)
 local current_width = vim.api.nvim_win_get_width(current_window)
 
-local float = vim.api.nvim_open_win(buf, true, {
-  row = current_height / 2 / 2,
-  col = current_width / 4,
-  height = math.floor(current_height / 2),
-  width = math.floor(current_width / 2),
-  border = border,
-  relative = "win",
-  title = "Attempt",
+local float_width = math.floor(current_width / 2)
+local float_height = math.floor(current_height / 2)
+
+local col = (current_width - float_width) / 2
+local row = (current_height - float_height) / 2
+
+local float_win = vim.api.nvim_open_win(buf, true, {
+	row = row,
+	col = col,
+	height = float_height,
+	width = float_width,
+	border = "rounded",
+	relative = "win",
+	title = "Attempt",
 })
 
--- vim.api.nvim_buf_set_text(float, 0, 0, end_row, end_col, replacement)
+-- vim.api.nvim_buf_set_text(float_win, 0, 0, end_row, end_col, replacement)
+local float_buff = vim.api.nvim_win_get_buf(float_win)
 
-vim.api.nvim_buf_set_lines(float, 0, -1, false, {
-  "this is a line",
-  "",
-  "so is this",
+vim.api.nvim_buf_set_lines(float_buff, 0, 3, false, {
+	"this is a line",
+	"",
+	"so is this",
 })
