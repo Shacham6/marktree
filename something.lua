4ec328fa1aa2481f006a651a72ed82db90793b1a
Shacham Ginat <shachamginat@Shachams-MacBook-Pro.local>
2025-01-25 15:34:19 +0200

    This works matey

--- something.lua
+++ something.lua
@@ -1,16 +1,40 @@
-local harpoon = require("harpoon")
+local function get_harpoon_marks_thing()
+	local harpoon = require("harpoon")
+	local mainlist = harpoon:list()
 
-local mainlist = harpoon:list()
+	local things = {}
 
-for _, item in ipairs(mainlist.items) do
-	vim.print(item)
+	for _, item in ipairs(mainlist.items) do
+		-- vim.print(item)
 
-	local bufnr = vim.fn.bufadd(item.value)
-	print(string.format("bufnr = %d", bufnr))
+		local bufnr = vim.fn.bufadd(item.value)
+		-- print(string.format("bufnr = %d", bufnr))
 
-	for mark_char_byte = string.byte("a"), string.byte("z"), 1 do
-		local mark_char = string.char(mark_char_byte)
-		local result = vim.api.nvim_buf_get_mark(bufnr, mark_char)
-		vim.print(string.format("%s = [%s] %s", item.value, mark_char, vim.inspect(result)))
+		local buffer_marks = {}
+
+		for mark_char_byte = string.byte("a"), string.byte("z"), 1 do
+			local mark_char = string.char(mark_char_byte)
+			local result = vim.api.nvim_buf_get_mark(bufnr, mark_char)
+
+			if result[1] == 0 and result[2] == 0 then
+				goto continue
+			end
+
+			-- vim.print(string.format("%s = [%s] %s", item.value, mark_char, vim.inspect(result)))
+			table.insert(buffer_marks, {
+				bufnr = bufnr,
+				filename = item.value,
+				position = result,
+				mark_char = mark_char,
+			})
+
+			::continue::
+		end
+		things[item.value] = buffer_marks
 	end
+
+	return things
 end
+
+local mark_things = get_harpoon_marks_thing()
+vim.print(mark_things)
