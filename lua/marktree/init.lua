7c4eb38280f8ae1c33ae50dce020511032837f0f
Shacham Ginat <shachamginat@Shachams-MacBook-Pro.local>
2025-01-26 11:47:02 +0200

    Hmm

--- lua/marktree/init.lua
+++ lua/marktree/init.lua
@@ -81,7 +81,6 @@ end
 
 function M.refresh()
 	local marks_loclist = get_harpoon_marks_loclist()
-	vim.print(marks_loclist)
 	insert_to_loclist(0, marks_loclist)
 	require("trouble").refresh("loclist")
 end
